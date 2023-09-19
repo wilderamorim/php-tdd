4ec48e36e0f003cbfef18c82ddeab53179266823
wilderamorim <wilderamorim@msn.com>
2023-09-19 20:55:29 -0300

    model test case

---
+++ tests/Unit/App/Models/UserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserTest extends ModelTestCase
+{
+    protected function model(): Model
+    {
+        return new \App\Models\User();
+    }
+
+    protected function expectedTraits(): array
+    {
+        return [
+            \Laravel\Sanctum\HasApiTokens::class,
+            \Illuminate\Database\Eloquent\Factories\HasFactory::class,
+            \Illuminate\Notifications\Notifiable::class,
+        ];
+    }
+
+    protected function expectedFillable(): array
+    {
+        return [
+            'name',
+            'email',
+            'password',
+        ];
+    }
+
+    protected function expectedCasts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
